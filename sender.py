e465c1e48781241125cd42e03d02ece037326f10
Adarsh Kumar <58614055+i-adarsh@users.noreply.github.com>
2021-01-01 10:54:25 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -3,30 +3,36 @@ import time
 from threading import *
 
 
-def show():
+def show(ip, port):
     myp = socket.SOCK_DGRAM
     afn = socket.AF_INET
     s = socket.socket(afn, myp)
-    ip = "192.168.99.106"
-    port = 6789
+    # ip = "192.168.99.106"
+    # port = 6789
     s.bind((ip, port))
     while True:
         x = s.recvfrom(1024)
         print('Received Message : ',x[0].decode())
         time.sleep(2 / 10)
 
-def reply():
+def reply(ip2, port2):
     myp = socket.SOCK_DGRAM
     afn = socket.AF_INET
     s = socket.socket(afn, myp)
     while True:
         msg = input()
-        s.sendto(msg.encode(), ("192.168.99.105", 3456))
+        # s.sendto(msg.encode(), ("192.168.99.105", 3456))
+        s.sendto(msg.encode(), (ip2, port2))
         print('Your Message : ', msg)
         time.sleep(0.2)
 
-t1 = Thread(target=reply)
-t2 = Thread(target=show)
+ip = input('Enter your IP address : ')
+port = int(input('Enter your port number : '))
+ip2 = input("Enter Friend's IP address : ")
+port2 = int(input("Enter Friend's port number : "))
+
+t1 = Thread(target=reply, args=(ip2, port2))
+t2 = Thread(target=show, args=(ip, port))
 
 t1.start()
 t2.start()
